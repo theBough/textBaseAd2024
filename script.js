724140232992c682ecc84cda1c7d4ffd3518c389
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-18 13:06:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let myOutput = document.getElementById("myOutput");
 let myInput = document.getElementById("myInput");
 
+let help = 
+  "***********************************</br>\
+   h - help</br>\
+   g - go</br>\
+   i - inventory</br>\
+   m - map</br>\
+   ************************************</br>"
+
 myInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        startGame()
-    }
+  if (event.key === "Enter") {
+    startGame()
+  }
 });
 
-function startGame(){
-  myOutput.innerHTML +=  " <br>You are in the commons at West K. You see a door to the north and a door to the east. Which way do you want to go?";
- 
+function startGame() {
+  myOutput.innerHTML += " <br>You are in the commons at West K. You see a door to the north and a door to the east. Which way do you want to go?";
+
+  if(myInput.value == 'h'){
+    myOutput.innerHTML += help;
+  }
+
 }
