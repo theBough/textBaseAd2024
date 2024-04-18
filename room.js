6f9d5e3f63346d2e7e256b9c3d2319f6ba38c8ab
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-18 13:31:49 -0500

    Update room.js

--- room.js
+++ room.js
@@ -9,8 +9,40 @@ function loadRooms(){
 }
 let room1 = {
   name: 'Anything you want',
-  description: `Describe the room here
-      I can use more lines
-      with no error
-      The back tick is the key above the TAB button `
+  description: `Describe the room here<br>
+      I can use more lines<br>
+      with no error<br>
+      The back tick is the key above the TAB button <br>`
+}
+let room2 = {
+  name: 'Anything you want',
+  description: ` `
+}
+let room3 = {
+  name: 'Anything you want',
+  description: ` `
+}
+let room4 = {
+  name: 'Anything you want',
+  description: ` `
+}
+let room5 = {
+  name: 'Anything you want',
+  description: ` `
+}
+let room6 = {
+  name: 'Anything you want',
+  description: ` `
+}
+let room7 = {
+  name: 'Anything you want',
+  description: ` `
+}
+let room8 = {
+  name: 'Anything you want',
+  description: ` `
+}
+let room9 = {
+  name: 'Anything you want',
+  description: ` `
 }
