e6467d22b0157974d056c33d35361d1634551e71
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-18 13:23:25 -0500

    Create room.js

---
+++ room.js
@@ -0,0 +1,16 @@
+let rooms = []
+
+function loadRooms(){
+  rooms = [
+    [room1, room2, room3],
+    [room4, room5, room6],
+    [room7, room8, room9],
+  ]
+}
+let room1 = {
+  name: 'Anything you want',
+  description: `Describe the room here
+      I can use more lines
+      with no error
+      The back tick is the key above the TAB button `
+}
