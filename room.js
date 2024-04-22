88d8822ce6d9fb2848d3b91d8259b5361f052500
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-22 12:42:21 -0500

    Update room.js

--- room.js
+++ room.js
@@ -16,33 +16,33 @@ let room1 = {
 }
 let room2 = {
   name: 'Anything you want',
-  description: ` `
+  description: `Room 2 `
 }
 let room3 = {
   name: 'Anything you want',
-  description: ` `
+  description: ` Room 3 `
 }
 let room4 = {
   name: 'Anything you want',
-  description: ` `
+  description: `<br> You are in Room 4`
 }
 let room5 = {
   name: 'Anything you want',
-  description: ` `
+  description: `Room 5 `
 }
 let room6 = {
   name: 'Anything you want',
-  description: ` `
+  description: ` Room 6`
 }
 let room7 = {
   name: 'Anything you want',
-  description: ` `
+  description: `Room 7 `
 }
 let room8 = {
   name: 'Anything you want',
-  description: ` `
+  description: `Room 8 `
 }
 let room9 = {
   name: 'Anything you want',
-  description: ` `
+  description: ` Room 9`
 }
