0dd13ad481d0cefdaeb3ced2e2aac08bf1c176dd
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-02 13:34:45 -0500

    Update items.js

--- items.js
+++ items.js
@@ -3,11 +3,13 @@ function resolveLook() {
     if (rooms[activeRow][activeCol].name == "roomOne") {
       myOutput.innerHTML += `<br> You notice a nice looking bicycle in the corner of the room. 
       You pick it up and put it in your backpack.`;
-      myItems.push("bike");
+       myOutput.scrollTop = myOutput.scrollHeight;
+      myItems.push("bicycle");
       console.log("you now have a bicycle")
     } else {
       myOutput.innerHTML += `<br> Looks like you are too
       far away from the bicycle.`;
-    }
-  }
+       myOutput.scrollTop = myOutput.scrollHeight;
+    }//end else
+  }//end item Bicycle
 }
