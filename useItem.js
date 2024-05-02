9717d39ea180eefeed5a486640d01ba119d69e8f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-02 13:35:10 -0500

    Update useItem.js

--- useItem.js
+++ useItem.js
@@ -3,15 +3,17 @@ function resolveUse(){
     if(myItems.includes('bicycle')){
       if(rooms[activeRow][activeCol].name == 'ramp'){
         myOutput.innerHTML += `<br> You ride your bicycle to the ramp.`;
-       
+         myOutput.scrollTop = myOutput.scrollHeight;
        //if you only want to use an item once. Then you would only need to remove it from the myItems   array. 
         //myItems.splice(myItems.indexOf('bicycle'),1);
       }else{
         //This means they have the item , but are in the wrong room
         myOutput.innerHTML += `<br> You take out your bike, go for a ride, and then nothing happens.`
+         myOutput.scrollTop = myOutput.scrollHeight;
       }
     }else{
       myOutput.innerHTML += `<br> Sorry, you do not have a bicycle.`
+       myOutput.scrollTop = myOutput.scrollHeight;
     }
   }//end item Bicycle
 }
